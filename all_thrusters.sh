493f8d2e3e6532be174f9149913760ebe1d1add5
William Barber <wobarber@outlook.com>
2025-11-17 23:20:41 -0800

    Update to Pico 2

--- all_thrusters.sh
+++ all_thrusters.sh
@@ -4,5 +4,5 @@ thrusters=(8 9 6 7 13 11 12 10)
 
 for i in {0..7}
 do
-	echo "Set ${thrusters[i]} PWM 1560" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e6635c08cb6b0b21-if00
+	echo "Set ${thrusters[i]} PWM 1560" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e66130100f198434-if00
 done
