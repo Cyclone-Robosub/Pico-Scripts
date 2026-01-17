8eb47d0ae60332bfa2261e53f74029ea0818c7f3
William Barber <wobarber@outlook.com>
2026-01-17 14:09:30 -0800

    Update to William's personal Pico

--- stop_thrusters.sh
+++ stop_thrusters.sh
@@ -4,5 +4,5 @@ thrusters=(8 9 6 7 13 11 12 10)
 
 for i in {0..7}
 do
-	echo "Set ${thrusters[i]} PWM 1500" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e66130100f198434-if00
+	echo "Set ${thrusters[i]} PWM 1500" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e663682593227739-if00
 done
--- thruster_test.sh
+++ thruster_test.sh
@@ -4,10 +4,10 @@ thrusters=(8 9 6 7 13 11 12 10)
 
 for i in {0..7}
 do
-	echo "Set ${thrusters[i]} PWM 1500" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e66130100f198434-if00
+	echo "Set ${thrusters[i]} PWM 1500" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e663682593227739-if00
 	sleep 1.5
-	echo "Set ${thrusters[i]} PWM 1560" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e66130100f198434-if00
+	echo "Set ${thrusters[i]} PWM 1560" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e663682593227739-if00
 	sleep 1
-	echo "Set ${thrusters[i]} PWM 1500" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e66130100f198434-if00
+	echo "Set ${thrusters[i]} PWM 1500" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e663682593227739-if00
 	sleep 0.5
 done
