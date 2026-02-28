1410fa5e096b0941240b39d4ea72f54270504cb1
CRS_AGGIES <crsaggies@gmail.com>
2026-02-27 17:48:04 -0800

    Updated Pico Serial-id

--- ping.sh
+++ ping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while ((1)); do
-	echo "ping" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_a81750b5305c8425-if00
+	echo "ping" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_7327d9a2ecd31892-if00
 	sleep 0.5
 done
