a57a4d05c563da8971b6ff0ed9a5849e0d40c83a
William Barber <wobarber@outlook.com>
2026-04-24 17:57:14 -0700

    Update GPIO number for dashboard script

--- Dashboard/dashboard_status.sh
+++ Dashboard/dashboard_status.sh
@@ -2,7 +2,7 @@
 #
 # Prints "1" if robot is software-killed, and "0" if robot is not software-killed.
 
-if [ $(gpio read 8) = 0 ] ; then
+if [ $(gpio read 9) = 0 ] ; then
     echo 1
 else
     echo 0
